04a7b1f657b65f8da8a6bd1ea36dbd2e164562f2
Chitranshu Srivastava <chitranshusjs@gmail.com>
2022-07-27 00:29:53 +0530

    Committing after adding a function in OneMoreClass

--- MyFirstDemoProject/src/com/sujata/demo/OneMoreClass.java
+++ MyFirstDemoProject/src/com/sujata/demo/OneMoreClass.java
@@ -2,5 +2,8 @@ package com.sujata.demo;
 //creating One More Class
 public class OneMoreClass {
 	//adding one new comment
+	public void funcOneMoreClass() {
+		System.out.println("Adding a function in one more class");
+	}
 
 }
